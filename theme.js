0d543b11873f101a10047c158b732973a6d7e5f3
Micky Socaci <micky@nowlive.ro>
2020-06-24 00:52:33 +0300

    Create theme.js

---
+++ theme.js
@@ -0,0 +1,4 @@
+const missingClassName = "container-xl";
+// find page head first div
+document.getElementsByClassName("pagehead")[0].firstElementChild.classList.add(missingClassName);
+document.getElementsByClassName("UnderlineNav")[0].classList.add(missingClassName);
