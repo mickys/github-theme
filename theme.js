cfec2a06d33aef4104311f644a40d68b34b218db
Micky Socaci <micky@nowlive.ro>
2020-06-24 13:56:45 +0300

    Class options

--- theme.js
+++ theme.js
@@ -1,4 +1,4 @@
-const missingClassName = "container-lg";
+const missingClassName = "container-lg"; // container-lx for wider screens
 // find page head first div
 document.getElementsByClassName("pagehead")[0].firstElementChild.classList.add(missingClassName);
 document.getElementsByClassName("UnderlineNav")[0].classList.add(missingClassName);
