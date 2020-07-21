cd02205895bc1c02929cf1fb5affeb24285c5691
Micky Socaci <micky@nowlive.ro>
2020-07-21 05:26:01 +0300

    Updated to handle new github layout

--- theme.js
+++ theme.js
@@ -1,5 +1,8 @@
-const missingClassName = "container-lg"; // container-lx for wider screens
-// find page head first div
-document.getElementsByClassName("pagehead")[0].firstElementChild.classList.add(missingClassName);
-document.getElementsByClassName("UnderlineNav")[0].classList.add(missingClassName);
-document.getElementsByClassName('repository-content')[0].classList.add(missingClassName);
+const missingClassName = "container-xl"; // container-lg for medium screens
+
+// find page head and fix it's children
+const headContainer = document.getElementById("js-repo-pjax-container");
+const elements = headContainer.children[0].children;
+for(let i = 0; i < elements.length; i++) {
+  elements[i].classList.add(missingClassName);
+}
